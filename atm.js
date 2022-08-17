354a70692ce9796fa4f25fa5c483a1509312208c
phryszlo <phryszlo@gmail.com>
2022-08-17 18:57:18 -0400

    there were mods

--- atm.js
+++ atm.js
@@ -1,13 +1,26 @@
 
+let cardInserted = false;
+let authenticated = false;
+let checkingBal = 550.64;
+let savingsBal = 2343.90;
+
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
   console.log("dom loaded");
   const cardSlot = document.querySelector(".cardSlot");
   cardSlot.addEventListener('click', insertCardClick);
+  const keys = document.querySelectorAll(".keys");
+  keys.forEach((key) => {
+    key.addEventListener('click', keyClick);
+  });
 } )
 
-let cardInserted = false;
+const keyClick = (e) => {
+  console.log(e.target.innerHTML);
+}
 
-function insertCardClick(e) {
+const insertCardClick = () =>  {
   //prevent multiple card insert clicks
   if (cardInserted) return;
   cardInserted = true;
@@ -18,4 +31,4 @@ function insertCardClick(e) {
   screen.style.filter = 'none';
   const screen2 = document.querySelector(".screenInside");
   screen2.innerHTML += "Welcome to A.T.M Bank. Thank you for inserting. ";
-}
+};
